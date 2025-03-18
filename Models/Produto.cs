968c9925095c546e4f72c96e60ab554a05b50b10
wenderson.farias <wenderson.farias10@gmail.com>
2025-03-18 19:25:16 -0300

    Add project files.

---
+++ Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFuncional.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public required string Nome { get; set; }
+
+        public decimal Preco { get; set; }
+    }
+}
